111f96020884359a0ace485970fae280f6074e9f
Jorge Alvarez <lvjorg@amazon.com>
2021-11-19 16:46:18 +0000

    Starting version of the code

---
+++ restaccess/restaccess/src/main/java/restservice/restaccess/RestaccessApplication.java
@@ -0,0 +1,13 @@
+package restservice.restaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaccessApplication.class, args);
+	}
+
+}
---
+++ restaccess/restaccess/src/test/java/restservice/restaccess/RestaccessApplicationTests.java
@@ -0,0 +1,13 @@
+package restservice.restaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
